649f9504e07d437c72f133f2bad5e1bab901fa05
Patrick Vermillion <36286874+Patrick1ruls@users.noreply.github.com>
2020-12-06 19:18:22 -0600

    Add loging (#10)

--- src/main/java/com/practice/javabean/Application.java
+++ src/main/java/com/practice/javabean/Application.java
@@ -1,7 +1,13 @@
 package com.practice.javabean;
 
+import java.util.logging.Logger;
+
+import static java.util.logging.Level.*;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Logger logger = Logger.getLogger(Application.class.getName());
+
+        logger.log(INFO, "Hello World!");
     }
 }
